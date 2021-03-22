1736810a2c62d785fcdc0ed488cfddf6541cc8db
Lucas Rosa <lucasrsilva1604@gmail.com>
2021-03-22 17:59:20 -0300

    Added better description to Options

--- Java Core Assessment/src/main/java/com/github/lucasrsa/App.java
+++ Java Core Assessment/src/main/java/com/github/lucasrsa/App.java
@@ -1,18 +1,19 @@
 package com.github.lucasrsa;
 
-import java.util.Locale;
+import java.util.Scanner;
 
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Running...");
+        Scanner sc = new Scanner(System.in);
         Options opt;
         do {
             System.out.println("Choose your option:");
             for (Options val : Options.values()) {
-                System.out.println("\t"+val+" or "+val.ordinal()+" to "+val.toString().toLowerCase());
+                System.out.println("\t"+val+" to "+val.describe());
             }
-            opt = Options.valueOf("EXIT");
+            System.out.print("-> ");
+            opt = Options.valueOf(sc.next().toUpperCase());
             switch (opt) {
                 case SAVE:
                     break;
