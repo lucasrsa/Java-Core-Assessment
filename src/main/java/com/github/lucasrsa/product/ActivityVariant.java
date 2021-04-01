3f6af762a68b02d205fe0f2d1af41c8e7ee3a89d
lucasrsa <lucasrsilva1604@gmail.com>
2021-04-01 12:24:10 -0300

    Implemented Activity to ActivityVariants

--- src/main/java/com/github/lucasrsa/product/ActivityVariant.java
+++ src/main/java/com/github/lucasrsa/product/ActivityVariant.java
@@ -3,10 +3,12 @@ package com.github.lucasrsa.product;
 import java.util.Scanner;
 
 public abstract class ActivityVariant extends Variant {
-    protected String Activity;
+    protected String activity;
 
     protected void setData(Scanner sc) {
         super.setData(sc);
 
+        System.out.print("Activity this product is destined to: ");
+        this.activity = sc.nextLine();
     }
 }
--- src/test/java/com/github/lucasrsa/AppTest.java
+++ src/test/java/com/github/lucasrsa/AppTest.java
@@ -25,8 +25,8 @@ public class AppTest {
                 "COLLECTION\nSUB\nCollection1\nSub-Collection1\n" +
                 "COLLECTION\nSUB\nWrongCollection\n" +
                 "PRODUCT\nSAVE\nCollection1\nSub-Collection1\nProduct1\nPrice\n9.99\nDescription\nQuantity\n10\nImage1\n\nTSHIRT\nColor1\nGender1\nWrongSize\nXXXL\nWrongSleeve\nLONG\n" +
-                "PRODUCT\nSAVE\nCollection1\nSub-Collection1\nProduct2\nPrice\n19.99\nDescription\nQuantity\n20\nImage1\n\nSNEAKERS\nColor1\nGender1\n" +
-                "PRODUCT\nSAVE\nCollection1\nSub-Collection1\nProduct3\nPrice\n29.99\nDescription\nQuantity\n30\nImage1\n\nPANTS\nColor1\nGender1\n" +
+                "PRODUCT\nSAVE\nCollection1\nSub-Collection1\nProduct2\nPrice\n19.99\nDescription\nQuantity\n20\nImage1\n\nSNEAKERS\nColor1\nGender1\nActivity1\n" +
+                "PRODUCT\nSAVE\nCollection1\nSub-Collection1\nProduct3\nPrice\n29.99\nDescription\nQuantity\n30\nImage1\n\nPANTS\nColor1\nGender1\nActivity1\n" +
                 "PRODUCT\nSAVE\nCollection1\nSub-Collection1\nProduct4\nPrice\n39.99\nDescription\nQuantity\n30\nImage1\n\nINCORRECT\n\n" +
                 "PRODUCT\nSAVE\nCollection1\nSub-Collection1\nProduct1\n" +
                 "PRODUCT\nSAVE\nCollection1\nWrongSub-Collection\n" +
